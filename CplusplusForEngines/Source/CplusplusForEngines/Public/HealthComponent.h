08611af28285f7942717e95e458d3972565a90b0
Alex Shaw <Ashaw9012@gmail.com>
2023-11-14 18:14:42 +0000

    made health component ready
    Health component is ready now time to mess around with input

--- CplusplusForEngines/Source/CplusplusForEngines/Public/HealthComponent.h
+++ CplusplusForEngines/Source/CplusplusForEngines/Public/HealthComponent.h
@@ -6,6 +6,7 @@
 #include "Components/ActorComponent.h"
 #include "HealthComponent.generated.h"
 
+DECLARE_DYNAMIC_MULTICAST_DELEGATE_OneParam(FComponentDeadSignature, AController*, causer); 
 
 UCLASS( ClassGroup=(Custom), meta=(BlueprintSpawnableComponent) )
 class CPLUSPLUSFORENGINES_API UHealthComponent : public UActorComponent
@@ -15,14 +16,33 @@ class CPLUSPLUSFORENGINES_API UHealthComponent : public UActorComponent
 public:	
 	// Sets default values for this component's properties
 	UHealthComponent();
-
-protected:
-	// Called when the game starts
-	virtual void BeginPlay() override;
-
-public:	
-	// Called every frame
+	
 	virtual void TickComponent(float DeltaTime, ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction) override;
 
-		
+	UPROPERTY(BlueprintAssignable, Category= "Components")
+	FComponentDeadSignature onComponentDead; 
+protected:
+	UPROPERTY(BlueprintReadOnly, VisibleAnywhere, Category= "Health")
+	float m_CurrentHealth;
+	UPROPERTY(BlueprintReadWrite, EditAnywhere, Category= "Health")
+	float m_MaxHealth; 
+
+	UPROPERTY(BlueprintReadOnly, VisibleAnywhere, Category= "Shield")
+	float m_CurrentShield;
+	UPROPERTY(BlueprintReadWrite, EditAnywhere, Category= "Shield")
+	float m_MaxShield;
+	UPROPERTY(BlueprintReadOnly, VisibleAnywhere, Category= "Shield")
+	bool m_CanRegenShield;
+	UPROPERTY(BlueprintReadOnly, VisibleAnywhere, Category= "Shield")
+	float m_ShieldRecoverDelayTimer;
+	UPROPERTY(BlueprintReadWrite, EditAnywhere, Category= "Shield")
+	float m_shieldRecoverRate;
+	UPROPERTY(BlueprintReadWrite, EditAnywhere, Category= "Shield")
+	float m_shieldRecoverDelay; 
+	
+	virtual void BeginPlay() override;
+
+	//function needed to bind to an event
+	UFUNCTION()
+	void DamageTaken(AActor* damagedActor, float damage, const UDamageType* damagetype, AController* instigator, AActor* causer);
 };
