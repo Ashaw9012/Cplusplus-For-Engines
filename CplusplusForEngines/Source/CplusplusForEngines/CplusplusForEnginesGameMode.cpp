d3d67c5938c6cdfef945213ce56f6be5fbea54c1
Alex Shaw <71638227+Ashaw9012@users.noreply.github.com>
2023-11-30 17:06:35 +0000

    added game mode

--- CplusplusForEngines/Source/CplusplusForEngines/CplusplusForEnginesGameMode.cpp
+++ CplusplusForEngines/Source/CplusplusForEngines/CplusplusForEnginesGameMode.cpp
@@ -45,40 +45,89 @@ void ACplusplusForEnginesGameMode::Logout(AController* Exiting)
 
 void ACplusplusForEnginesGameMode::DecreaseCountdown()
 {
-	
+	_CountdownTimer--;
+	GEngine->AddOnScreenDebugMessage(-1, 5.f, FColor::Green, FString::Printf(TEXT("%d"), _CountdownTimer));
+	if(_CountdownTimer == 0)
+	{
+		StartMatch(); 
+	}
+	else
+	{
+		GetWorld()->GetTimerManager().SetTimer(_TimerDecreaseCountdown, this, &ACplusplusForEnginesGameMode::DecreaseCountdown, 1.f, false);
+	}
 }
 
 void ACplusplusForEnginesGameMode::BeginPlay()
 {
 	Super::BeginPlay();
+
+	//TODO: Add Gamerule Stuff here
 }
 
 void ACplusplusForEnginesGameMode::HandleMatchIsWaitingToStart()
 {
+	GetWorld()->GetTimerManager().SetTimer(_TimerDecreaseCountdown, this, &ACplusplusForEnginesGameMode::DecreaseCountdown, 1.f, false);
 	Super::HandleMatchIsWaitingToStart();
 }
 
 void ACplusplusForEnginesGameMode::HandleMatchHasStarted()
 {
+	for (AController* controller : _PlayerControllers)
+	{
+		if(UKismetSystemLibrary::DoesImplementInterface(controller, UMatchStateHandler::StaticClass()))
+		{
+			IMatchStateHandler::Execute_Handle_MatchStarted(controller);
+		}
+	}
+	
 	Super::HandleMatchHasStarted();
 }
 
 void ACplusplusForEnginesGameMode::HandleMatchHasEnded()
 {
+	for (AController* controller : _PlayerControllers)
+	{
+		if(UKismetSystemLibrary::DoesImplementInterface(controller, UMatchStateHandler::StaticClass()))
+		{
+			IMatchStateHandler::Execute_Handle_MatchEnded(controller);
+		}
+	}
 	Super::HandleMatchHasEnded();
 }
 
 void ACplusplusForEnginesGameMode::OnMatchStateSet()
 {
+	FString output;
+	
+	if(MatchState == MatchState::WaitingToStart)
+	{
+		output = "waiting to start";
+	}
+	else if (MatchState == MatchState::InProgress)
+	{
+		output = "In progress";
+	}
+	else if(MatchState == MatchState::InProgress)
+	{
+		output = "In progress";
+	}
+	else if (MatchState == MatchState::LeavingMap)
+	{
+		output = "Leaving map";
+	}
+	
+	//shows the right message and puts it as a char array
+	GEngine->AddOnScreenDebugMessage(-1, 5.f, FColor::Red, FString::Printf(TEXT("State change to: %s"), *output));
+	
 	Super::OnMatchStateSet();
 }
 
 bool ACplusplusForEnginesGameMode::ReadyToStartMatch_Implementation()
 {
-	return Super::ReadyToStartMatch_Implementation();
+	return false;
 }
 
 bool ACplusplusForEnginesGameMode::ReadyToEndMatch_Implementation()
 {
-	return Super::ReadyToEndMatch_Implementation();
+	return false;
 }
