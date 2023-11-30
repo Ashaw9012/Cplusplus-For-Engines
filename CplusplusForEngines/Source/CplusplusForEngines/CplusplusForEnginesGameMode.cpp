e68595e663de5c0f05c3b464d27c2c96f7f94b6f
Alex Shaw <71638227+Ashaw9012@users.noreply.github.com>
2023-11-30 14:54:52 +0000

    game modes

--- CplusplusForEngines/Source/CplusplusForEngines/CplusplusForEnginesGameMode.cpp
+++ CplusplusForEngines/Source/CplusplusForEngines/CplusplusForEnginesGameMode.cpp
@@ -1,14 +1,84 @@
-// Copyright Epic Games, Inc. All Rights Reserved.
-
 #include "CplusplusForEnginesGameMode.h"
 #include "CplusplusForEnginesCharacter.h"
+#include "GEPPlayerController.h"
+#include "GameFramework/PlayerStart.h"
+#include "Kismet/GameplayStatics.h"
 #include "UObject/ConstructorHelpers.h"
 
 ACplusplusForEnginesGameMode::ACplusplusForEnginesGameMode()
 	: Super()
 {
-	// set default pawn class to our Blueprinted character
-	static ConstructorHelpers::FClassFinder<APawn> PlayerPawnClassFinder(TEXT("/Game/FirstPerson/Blueprints/BP_FirstPersonCharacter"));
-	DefaultPawnClass = PlayerPawnClassFinder.Class;
+	_CountdownTimer = 3;
+	
+}
 
+AActor* ACplusplusForEnginesGameMode::FindPlayerStart_Implementation(AController* Player, const FString& IncomingName)
+{
+	if(_PlayerStarts.Num() == 0)
+	{
+		UGameplayStatics::GetAllActorsOfClass(GetWorld(), APlayerStart::StaticClass(), _PlayerStarts);
+	}
+	if(_PlayerStarts.Num() > 0)
+	{
+		return _PlayerStarts[0];
+	}
+	return nullptr; 
+}
+
+void ACplusplusForEnginesGameMode::PostLogin(APlayerController* NewPlayer)
+{
+	_PlayerControllers.AddUnique(NewPlayer);
+	if(AGEPPlayerController* castedPC = Cast<AGEPPlayerController>(NewPlayer))
+	{
+		//TODO: bind to any relevant events
+		castedPC->Init(); 
+	}
+	
+	Super::PostLogin(NewPlayer);
+}
+
+void ACplusplusForEnginesGameMode::Logout(AController* Exiting)
+{
+	_PlayerControllers.Remove(Exiting);
+	Super::Logout(Exiting);
+}
+
+void ACplusplusForEnginesGameMode::DecreaseCountdown()
+{
+	
+}
+
+void ACplusplusForEnginesGameMode::BeginPlay()
+{
+	Super::BeginPlay();
+}
+
+void ACplusplusForEnginesGameMode::HandleMatchIsWaitingToStart()
+{
+	Super::HandleMatchIsWaitingToStart();
+}
+
+void ACplusplusForEnginesGameMode::HandleMatchHasStarted()
+{
+	Super::HandleMatchHasStarted();
+}
+
+void ACplusplusForEnginesGameMode::HandleMatchHasEnded()
+{
+	Super::HandleMatchHasEnded();
+}
+
+void ACplusplusForEnginesGameMode::OnMatchStateSet()
+{
+	Super::OnMatchStateSet();
+}
+
+bool ACplusplusForEnginesGameMode::ReadyToStartMatch_Implementation()
+{
+	return Super::ReadyToStartMatch_Implementation();
+}
+
+bool ACplusplusForEnginesGameMode::ReadyToEndMatch_Implementation()
+{
+	return Super::ReadyToEndMatch_Implementation();
 }
--- CplusplusForEngines/Source/CplusplusForEngines/CplusplusForEnginesGameMode.h
+++ CplusplusForEngines/Source/CplusplusForEngines/CplusplusForEnginesGameMode.h
@@ -1,18 +1,45 @@
-// Copyright Epic Games, Inc. All Rights Reserved.
-
 #pragma once
 
 #include "CoreMinimal.h"
-#include "GameFramework/GameModeBase.h"
+#include "GameFramework/GameMode.h"
 #include "CplusplusForEnginesGameMode.generated.h"
 
-UCLASS(minimalapi)
-class ACplusplusForEnginesGameMode : public AGameModeBase
+UCLASS(minimalapi, Abstract)
+class ACplusplusForEnginesGameMode : public AGameMode
 {
 	GENERATED_BODY()
 
 public:
 	ACplusplusForEnginesGameMode();
+
+	virtual AActor* FindPlayerStart_Implementation(AController* Player, const FString& IncomingName) override;
+
+	virtual void PostLogin(APlayerController* NewPlayer) override;
+	virtual void Logout(AController* Exiting) override;
+
+protected:
+	TArray<TObjectPtr<AActor>> _PlayerStarts;
+
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly)
+	TArray<TObjectPtr<AController>> _PlayerControllers;
+
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category=MatchManagement)
+	int _CountdownTimer;
+
+	FTimerHandle _TimerDecreaseCountdown;
+	UFUNCTION()
+	void DecreaseCountdown();
+
+	virtual void BeginPlay() override;
+
+	virtual void HandleMatchIsWaitingToStart() override;
+	virtual void HandleMatchHasStarted() override;
+	virtual void HandleMatchHasEnded() override;
+
+	virtual void OnMatchStateSet() override;
+
+	virtual bool ReadyToStartMatch_Implementation() override;
+	virtual bool ReadyToEndMatch_Implementation() override;
 };
 
 
